8b1e840ca633ff2fb110c152a3c48a7eb4c47f0b
DangThanh2010 <57797011+DangThanh2010@users.noreply.github.com>
2021-01-08 20:16:33 +0700

    Don't add order with admin's account

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,30 +7,35 @@ module.exports.addOrder = async (req, res, next) =>
     {
         if(req.user)
         {
-            let address = req.body.address;
-            let phonenum = req.body.phonenum;
-            let checkPhonenum = true;
-
-            if(phonenum.length !== 10 && phonenum.length !== 11)
-                checkPhonenum = false;
-
-            if(phonenum.charAt(0) !== '0')
-                checkPhonenum = false;
-
-            for(let i = 0; i < phonenum.length; i++)
-            {
-                if(phonenum.charAt(i) < '0' || phonenum.charAt(i) > '9')
-                    checkPhonenum = false;
-            }
-            if(checkPhonenum)
-            {
-                await orderModel.addOrder(req.session.cart, req.user, address, phonenum);
-                req.session.cart = null;
-                res.redirect('/');
-            }
+            if(req.user.is_admin)
+                res.redirect('/'); 
             else
             {
-                res.redirect("/cart");
+                let address = req.body.address;
+                let phonenum = req.body.phonenum;
+                let checkPhonenum = true;
+
+                if(phonenum.length !== 10 && phonenum.length !== 11)
+                    checkPhonenum = false;
+
+                if(phonenum.charAt(0) !== '0')
+                    checkPhonenum = false;
+
+                for(let i = 0; i < phonenum.length; i++)
+                {
+                    if(phonenum.charAt(i) < '0' || phonenum.charAt(i) > '9')
+                        checkPhonenum = false;
+                }
+                if(checkPhonenum)
+                {
+                    await orderModel.addOrder(req.session.cart, req.user, address, phonenum);
+                    req.session.cart = null;
+                    res.redirect('/');
+                }
+                else
+                {
+                    res.redirect("/cart");
+                }
             }
         }
         else
